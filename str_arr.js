d9f7a695253e57baba84b836541bd58ec0f230ba
FARAJ ELMADHI <63078234+FarajElmadhi@users.noreply.github.com>
2021-09-24 21:59:38 +0300

    Assignment 1

---
+++ str_arr.js
@@ -0,0 +1,40 @@
+
+// -------------| 1 |-----
+
+let str = "abcdefghiklmnopqrstuvxyz1234567890"
+
+console.log();  //  string
+
+console.log()   //  klmno
+
+console.log();  //  34 
+
+console.log();  //   7
+
+console.log();  //   ABCDEFGHIKLMNOPQRSTUVXYZ1234567890
+
+console.log();  // 0987654321zyxvutsrqponmlkihgfedcba
+
+
+
+
+
+
+// -------------| 2 |-----
+// استخدم المتغيرات التالية لاخراج -نص- كما هو موضح
+
+let probleme = "The compiler raised 469 error after modify code";
+let solution = ["Ctrl", "+", "Z"];
+
+// 
+
+
+console.log(); //  Probleme: compiler raised 469 error and warning. Solution: Ctrl+Z
+
+
+
+
+
+
+
+
